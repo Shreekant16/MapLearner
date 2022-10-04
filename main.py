fe3f8cb4a2cc33f3a3fc0bf068119040a29a5349
Shreekant Suhas Pukale <111984571+Shreekant16@users.noreply.github.com>
2022-10-04 12:07:31 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+from turtle import Turtle, Screen
+import turtle
+import pandas
+screen = Screen()
+# def home(x,y):
+#     print(x,y)
+# screen.onscreenclick(home)
+
+image = "map.png"
+screen.bgpic(picname=image)
+data = pandas.read_csv('data.csv')
+states = data.state.to_list()
+end = False
+score = 0
+while not end:
+    user = turtle.textinput(title="STATE NAME", prompt="Write the state name : ")
+    if user == 'off':
+        print(f"{score}/29")
+        end = True
+    elif user in states:
+        score += 1
+        state_data = data[data.state == user]
+        shree = Turtle()
+        shree.penup()
+        shree.goto(int(state_data.x), int(state_data.y))
+        shree.write(user)
+        shree.hideturtle()
+
+
+screen.mainloop()
